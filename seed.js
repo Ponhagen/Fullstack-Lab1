d43451d1a2079e6607c10359d6c8bb2f72d6831e
Pontus <157807027+Ponhagen@users.noreply.github.com>
2025-04-02 13:01:09 +0200

    Tillagda kommentarer

--- seed.js
+++ seed.js
@@ -1,3 +1,3 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const Dish = require('.models/dish');
+const mongoose = require('mongoose'); // Kopplar biblotek till Node,js till MongoDB
+require('dotenv').config();           // Kopplar till .env filen
+const Dish = require('.models/dish'); // Kopplar till modellen Dish min MongoDB kollektion
