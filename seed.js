26861afe7c3379ab6e20eefe581ca50cf2a6320c
Pontus <157807027+Ponhagen@users.noreply.github.com>
2025-04-03 14:02:59 +0200

    Första rätten adderad

--- seed.js
+++ seed.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose'); // Kopplar biblotek till Node,js till MongoDB
 require('dotenv').config();           // Kopplar till .env filen
 const Dish = require('.models/dish'); // Kopplar till modellen Dish min MongoDB kollektion
+
+const dishes = [
+    {
+        name: 'Spaghetti Köttfärsås',
+        ingredients: ['Spaghetti', 'Köttfärs', 'Tomatsås', 'Lök', ],
+        preperationSteps: ['Salta vatten','Koka upp vatten', 'Hacka lök','Stek färsen', 'Tillsätt tomatsås', 'Koka spaghettin', 'Låt det puttra medans vi väntar på spaghettin'],
+        cookingTime: 30,
+        origin: 'Italien',
+        difficulty: 'Medium',
+
+    },
+]
